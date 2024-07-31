dfab039383becfbbc053c03428abe53ce3456dcf
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-07-31 17:51:41 +0530

    auth controller added

---
+++ controllers/auth_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from ..models.user_model import User
+from .. import db
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully!'})
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Invalid credentials!'}), 401
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'access_token': access_token})
