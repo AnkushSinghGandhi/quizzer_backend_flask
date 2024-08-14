a658dfcbd5c2d2d340ba965f60bbfdec31f0c2e7
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-08-14 21:47:13 +0530

    fixed the admin only decorator function

--- decorators.py
+++ decorators.py
@@ -1,15 +1,25 @@
 from functools import wraps
 from flask_jwt_extended import get_jwt_identity
-from flask import jsonify
+from flask import jsonify, request
 from models.user_model import User
 
 def admin_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        user_id = get_jwt_identity()
-        user = User.query.get(user_id)
-        if user and user.role == 'admin':
-            return fn(*args, **kwargs)
-        else:
-            return jsonify({'message': 'Admins only!'}), 403
+        try:
+            identity = get_jwt_identity()
+            
+            user_id = identity.get('id') if isinstance(identity, dict) else identity
+            user = User.query.get(user_id)
+
+            if user and user.role == 'admin':
+                return fn(*args, **kwargs)
+            else:
+                return jsonify({'message': 'Admins only!'}), 403
+        except Exception as e:
+            print(f"Error in admin_required decorator: {e}")
+            return jsonify({'message': 'Authorization error!'}), 403
+        
+
+
     return wrapper
